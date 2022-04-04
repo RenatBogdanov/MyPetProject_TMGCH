23605835eb53ee9946572f2c7a0a151076ad95a3
RenatBogdanov <zeloniyosminog@gmail.com>
2022-04-04 21:03:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import tkinter as tk
 from tkinter import *
 import random
 
+#Открытие игрового окна (2)
+def startGame():
+    print("Starting game")
+    firstW.destroy()
+    secondW = tk.Tk()
+    charName = tk.Label(text = name, fg = 'Black')
+    charName.pack()
+    secondW.mainloop()    
 
-window = tk.Tk()
- 
-label = tk.Label(
-    text="Hi",
-    fg="white",
-    bg="white",
-    width=20,
-    height=20
+firstW = tk.Tk()
+
+hello = tk.Label(
+    text="Hello, my name is...",
+    fg="#003844",
+    bg="#FFB100"
 )
- 
-label.pack()
 
-def start_window():
-	
+inputName = tk.Entry()
+
+name = inputName.get()
+
+playBt = tk.Button(
+    text=("Let's start"),
+    bg="#F194B4",
+    fg="#003844",
+    command=startGame
+)
 
 
-window.mainloop()
+
+hello.pack()
+inputName.pack()
+playBt.pack()
+firstW.mainloop()
+
