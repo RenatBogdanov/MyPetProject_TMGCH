20273985ec0254e164c85ccdf379bcbbcd29998f
RenatBogdanov <zeloniyosminog@gmail.com>
2022-04-04 18:20:03 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import *
+import random
+
+
+window = tk.Tk()
+ 
+label = tk.Label(
+    text="Hi",
+    fg="white",
+    bg="white",
+    width=20,
+    height=20
+)
+ 
+label.pack()
+
+def start_window():
+	
+
+
+window.mainloop()
